88886330ecd608587e18b69953b0388879033bc1
Adam <>
2023-11-18 17:05:38 +0300

    update docs

--- script.el
+++ script.el
@@ -1,5 +1,17 @@
-(let ((directory "stuff"))
-  (mapcar #'org-babel-load-file
-		  (mapcar (apply-partially #'concat (getenv "PWD") "/")
-				  (mapcar (apply-partially #'concat (concat directory "/"))
-						  (remove "." (remove ".." (directory-files (concat "./" directory))))))))
+(let* ((current-working-directory (if (display-graphic-p)
+									  (file-name-directory buffer-file-name)
+									(concat (getenv "PWD") "/")))
+	   (files-in-directory
+		(lambda (directory)
+		  (mapcar (apply-partially #'concat (concat directory "/"))
+				  (remove "." (remove ".." (directory-files (concat "./" directory)))))))
+	   (full-files-names
+		(lambda (files-strings)
+		  (mapcar (apply-partially #'concat current-working-directory)
+				  files-strings)))
+	   (org-to-el
+		(lambda (org-files)
+		  (mapcar #'org-babel-load-file org-files))))
+  (funcall org-to-el
+		   (funcall full-files-names
+					(cons "stuff.org" (funcall files-in-directory "stuff")))))
